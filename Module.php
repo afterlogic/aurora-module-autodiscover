928c2ba9b7a3bcb7fabf92e06eb37368b878a8e9
sash04ek <a.ovcharov@gmail.com>
2026-07-08 10:07:06 +0300

    Improve autodiscover response schema handling and generate XML with DOMDocument

--- Module.php
+++ Module.php
@@ -66,47 +66,87 @@ class Module extends \Aurora\System\Module\AbstractModule
 
     public function EntryAutodiscover()
     {
-        $sInput = \file_get_contents('php://input');
+        // List of allowed AcceptableResponseSchema values. All others will be treated as absent.
+        static $aAllowedSchemas = [
+            'http://schemas.microsoft.com/exchange/autodiscover/outlook/responseschema/2006a',
+            'http://schemas.microsoft.com/exchange/autodiscover/mobilesync/responseschema/2006',
+            'http://schemas.microsoft.com/exchange/autodiscover/responseschema/2006',
+        ];
 
+        $sInput = \file_get_contents('php://input');
         \Aurora\System\Api::Log('#autodiscover:');
         \Aurora\System\Api::LogObject($sInput);
 
         $aMatches = array();
         $aEmailAddress = array();
         \preg_match("/\<AcceptableResponseSchema\>(.*?)\<\/AcceptableResponseSchema\>/i", $sInput, $aMatches);
-        \preg_match("/\<EMailAddress\>(.*?)\<\/EMailAddress\>/", $sInput, $aEmailAddress);
-        if (!empty($aMatches[1]) && !empty($aEmailAddress[1])) {
-            $sAutodiscover = self::Decorator()->GetAutodiscover($aEmailAddress[1]);
+        \preg_match("/\<EMailAddress\>(.*?)\<\/EMailAddress\>/i", $sInput, $aEmailAddress);
 
-            $sResult = \implode("\n", array(
-'<Autodiscover xmlns="http://schemas.microsoft.com/exchange/autodiscover/responseschema/2006">',
-'	<Response xmlns="' . $aMatches[1] . '">',
-$sAutodiscover,
-'	</Response>',
-'</Autodiscover>'));
+        // Schema validation: if AcceptableResponseSchema is present, it must be one of the allowed values. Otherwise, treat as absent.
+        $sSchema = '';
+        if (!empty($aMatches[1])) {
+            $sCandidate = \trim($aMatches[1]);
+            if (\in_array($sCandidate, $aAllowedSchemas, true)) {
+                $sSchema = $sCandidate;
+            } else {
+                \Aurora\System\Api::Log('Autodiscover: rejected unknown AcceptableResponseSchema value: ' . $sCandidate);
+            }
         }
 
-        if (empty($sResult)) {
+        $sEmail = !empty($aEmailAddress[1]) ? \trim($aEmailAddress[1]) : '';
+
+        $oDoc = new \DOMDocument('1.0', 'utf-8');
+        $oDoc->formatOutput = false;
+
+        $oRoot = $oDoc->createElement('Autodiscover');
+        $oRoot->setAttribute('xmlns', 'http://schemas.microsoft.com/exchange/autodiscover/responseschema/2006');
+        $oDoc->appendChild($oRoot);
+
+        $oResponse = $oDoc->createElement('Response');
+        // xmlns added as attribute to Response element, not Autodiscover, because some clients (e.g. Outlook 2016) ignore the outer xmlns and fail to parse the response if it's not present on Response.
+        if ($sSchema !== '') {
+            $oResponse->setAttribute('xmlns', $sSchema);
+        }
+        $oRoot->appendChild($oResponse);
+
+        $bOk = false;
+        if ($sSchema !== '' && $sEmail !== '') {
+            $sAutodiscover = self::Decorator()->GetAutodiscover($sEmail);
+            if (!empty($sAutodiscover)) {
+                // GetAutodiscover() returns trusted server-generated XML (not user input),
+                // so it is appended as an XML fragment instead of escaped text.
+                $oFragment = $oDoc->createDocumentFragment();
+                // appendXML() validates that the fragment is well-formed XML.
+                if (@$oFragment->appendXML($sAutodiscover)) {
+                    $oResponse->appendChild($oFragment);
+                    $bOk = true;
+                } else {
+                    \Aurora\System\Api::Log('Autodiscover: GetAutodiscover() returned malformed XML fragment');
+                }
+            }
+        }
+
+        if (!$bOk) {
             $usec = $sec = 0;
             list($usec, $sec) = \explode(' ', \microtime());
-            $sResult = \implode("\n", array('<Autodiscover xmlns="http://schemas.microsoft.com/exchange/autodiscover/responseschema/2006">',
-(
-    empty($aMatches[1]) ?
-'	<Response>' :
-'	<Response xmlns="' . $aMatches[1] . '">'
-),
-'		<Error Time="' . \gmdate('H:i:s', $sec) . \substr($usec, 0, \strlen($usec) - 2) . '" Id="2477272013">',
-'			<ErrorCode>600</ErrorCode>',
-'			<Message>Invalid Request</Message>',
-'			<DebugData />',
-'		</Error>',
-'	</Response>',
-'</Autodiscover>'));
+
+            $oError = $oDoc->createElement('Error');
+            $oError->setAttribute('Time', \gmdate('H:i:s', $sec) . \substr($usec, 0, \strlen($usec) - 2));
+            $oError->setAttribute('Id', '2477272013');
+
+            $oErrorCode = $oDoc->createElement('ErrorCode', '600');
+            $oMessage = $oDoc->createElement('Message', 'Invalid Request');
+            $oDebugData = $oDoc->createElement('DebugData');
+
+            $oError->appendChild($oErrorCode);
+            $oError->appendChild($oMessage);
+            $oError->appendChild($oDebugData);
+
+            $oResponse->appendChild($oError);
         }
 
-        \header('Content-Type: text/xml');
-        $sResult = '<' . '?xml version="1.0" encoding="utf-8"?' . '>' . "\n" . $sResult;
-
+        \header('Content-Type: text/xml; charset=utf-8');
+        $sResult = $oDoc->saveXML();
         echo $sResult;
 
         \Aurora\System\Api::Log('');
